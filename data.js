1dacd6fb20a3b9252970e12d596520a7261a4316
allex <allexissoares@gmail.com>
2020-06-12 00:05:45 -0300

    changes: create a little database

---
+++ data.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id:  'starter',
+    modules: '05 Módulos',
+    price: 'Gratuito'
+  },
+  {
+    id:  'launchbase',
+    modules: '10 Módulos',
+    price: 'Pago'
+  },
+  {
+    id:  'gostack',
+    modules: '14 Módulos',
+    price: 'Pago'
+  }
+]
--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 
+const data = require('./data')
+
 const app = express()
 app.use(express.static('public'))
 
 app.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-  express: app
+  express: app,
+  autoescape: false
 })
 
 app.get('/', function(req, res) {
-  return res.render('about')
+  const info = {
+    company: 'Rocketseat',
+    description: 'A empresa que decola os programadores!',
+    techs:[
+      { name: 'JavaScript' },
+      { name: 'NodeJS' },
+      { name: 'React' },
+      { name: 'React Native' }
+    ],
+    links: [
+      { link_url: 'https://www.facebook.com/rocketseat', name: 'Facebook' },
+      { link_url: 'https://www.instagram.com/rocketseat_oficial', name: 'Instagram' },
+      { link_url: 'https://github.com/rocketseat', name: 'Github' }
+    ]
+  }
+
+  return res.render('about', { info })
 })
 
-app.get('/courses', function(req, res) {
-  return res.render('courses')
+app.get('/courses', function(req, res) {  
+  return res.render('courses', { courses: data })
 })
 
 app.use(function(req, res) {
